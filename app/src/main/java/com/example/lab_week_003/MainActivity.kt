2b36d3f78f349837f89816ccec3858899461d477
LianSinaga21 <parsauliansinaga17@gmail.com>
2025-12-11 22:25:39 +0700

    Commit No. 1 – add fragment lifecyle logging

--- app/src/main/java/com/example/lab_week_003/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_003/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.lab_week_003
 
 import android.os.Bundle
+import android.util.Log    // <-- tambahkan ini!
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity", "App started") // contoh pemakaian Log
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
